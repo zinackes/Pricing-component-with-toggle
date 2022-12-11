8cbd91c190d78b7d89e772399f8c7a2ba2292b24
zinacke <88772783+zinacke@users.noreply.github.com>
2022-12-11 12:07:52 +0100

    g

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const master = document.getElementById("master");
 function PriceChange(checkbox){
 
     if (checkbox.checked){
-        Basic.textContent = '19.99';
+        Basic.innerHTML = '19.99';
+        Pro.innerHTML = '24.99';
+        master.innerHTML = '39.99';
+    }
+    else{
+        Basic.innerHTML = '199.99';
+        Pro.innerHTML = '249.99';
+        master.innerHTML = '399.99';
     }
 }
